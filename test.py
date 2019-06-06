81feb326c6d46ae090b74374c1a9cbc651f93f13
vitaly.r <vitaly.r@mail.huji.ac.il>
2019-06-06 22:51:56 +0300

    pre-processing needs fixing to saving

--- test.py
+++ test.py
@@ -3,4 +3,6 @@ from PreProcessor import PreProcessor
 
 if __name__ == '__main__':
     processed_data, labels = PreProcessor.pre_process()
+    processed_data.to_csv('processed/processed_data.csv')
+    labels.to_csv('processed/processed_labels.csv')
     print('done!')
