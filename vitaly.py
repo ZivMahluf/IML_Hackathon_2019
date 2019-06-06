88dfa4f1a9a103b911f80de39dbff50d505343d2
vitaly.r <vitaly.r@mail.huji.ac.il>
2019-06-06 12:16:22 +0300

    advanced tweet dataframe parsing

--- vitaly.py
+++ vitaly.py
@@ -7,24 +7,42 @@ from os.path import isfile, join
 
 
 #  break a tweet to its base features as selected beforehand
-def process_sentence(tweet):
-    tweet = str(tweet)
-    st = re.sub(r"^\[[\"|\']", r'', tweet)
-    st = re.sub(r"[\"|\']\]$", r'', st)
-    has_link = 1 if re.search(r"http[s]?://[A-Za-z0-9\.\/]*\s*", st) is not None else 0
-    st = re.sub(r"http[s]?://[A-Za-z0-9\.\/]*\s*", r"", st)
-    is_rt = 1 if re.search(r"^RT *@[\w]*:", st) is not None else 0
-    st = re.sub(r"^RT *@[\w]*:", r'', st)
-    has_dots = 1 if 0 < st.count('.') else 0
-    has_tags = 1 if 0 < st.count('@') else 0
-    has_hashs = 1 if 0 < st.count('#') else 0
-    has_exc = 1 if 0 < st.count('!') else 0
-    has_qmark = 1 if 0 < st.count('?') else 0
-    st = re.sub(r"\s+", r' ', st)
-    st = re.sub(r"[\.,]", r'', st)
-    st = st.encode('ascii', 'ignore').decode('ascii')
+def process_sentence(tweets):
+    result = pd.DataFrame()
+    str_tweets = tweets.apply(lambda tweet: str(tweet))
+    filtered = str_tweets.apply(lambda tweet: tweet[2:-1])
+    filtered = filtered.apply(lambda tweet: re.sub(r"^\[[\"|\']", r'', tweet))
+    filtered = filtered.apply(lambda tweet: re.sub(r"[\"|\']\]$", r'', tweet))
+    result['has_link'] = filtered.apply(lambda tweet: 1 if re.search(r"http[s]?://[A-Za-z0-9\.\/]*\s*", tweet) is not None else 0)
+    filtered = filtered.apply(lambda tweet: re.sub(r"http[s]?://[A-Za-z0-9\.\/]*\s*", r"", tweet))
+    result['is_RT'] = filtered.apply(lambda tweet: 1 if re.search(r"^RT *@[\w]*:", tweet) is not None else 0)
+    filtered = filtered.apply(lambda tweet: re.sub(r"^RT *@[\w]*:", r'', tweet))
+    result['has_dots'] = filtered.apply(lambda tweet: 1 if 0 < tweet.count('.') else 0)
+    result['has_tags'] = filtered.apply(lambda tweet: 1 if 0 < tweet.count('.') else 0)
+    result['has_hash'] = filtered.apply(lambda tweet: 1 if 0 < tweet.count('.') else 0)
+    result['has_exmark'] = filtered.apply(lambda tweet: 1 if 0 < tweet.count('.') else 0)
+    result['has_qmark'] = filtered.apply(lambda tweet: 1 if 0 < tweet.count('.') else 0)
+    filtered = filtered.apply(lambda tweet: re.sub(r"\s+", r' ', tweet))
+    filtered = filtered.apply(lambda tweet: re.sub(r"[\.,]", r'', tweet))
+    filtered = filtered.apply(lambda tweet: tweet.encode('ascii', 'ignore').decode('ascii'))
+    result['words'] = filtered.apply(lambda tweet: tweet.split())
+    return result
+    # st = re.sub(r"^\[[\"|\']", r'', '')
+    # st = re.sub(r"[\"|\']\]$", r'', st)
+    # has_link = 1 if re.search(r"http[s]?://[A-Za-z0-9\.\/]*\s*", st) is not None else 0
+    # st = re.sub(r"http[s]?://[A-Za-z0-9\.\/]*\s*", r"", st)
+    # is_rt = 1 if re.search(r"^RT *@[\w]*:", st) is not None else 0
+    # st = re.sub(r"^RT *@[\w]*:", r'', st)
+    # has_dots = 1 if 0 < st.count('.') else 0
+    # has_tags = 1 if 0 < st.count('@') else 0
+    # has_hashs = 1 if 0 < st.count('#') else 0
+    # has_exc = 1 if 0 < st.count('!') else 0
+    # has_qmark = 1 if 0 < st.count('?') else 0
+    # st = re.sub(r"\s+", r' ', st)
+    # st = re.sub(r"[\.,]", r'', st)
+    # st = st.encode('ascii', 'ignore').decode('ascii')
     # print(st.split())
-    return [is_rt, has_link, has_dots, has_exc, has_tags, has_hashs, has_qmark, st.split()]
+    # return [is_rt, has_link, has_dots, has_exc, has_tags, has_hashs, has_qmark, st.split()]
 
 
 if __name__ == '__main__':
@@ -38,6 +56,8 @@ if __name__ == '__main__':
     # minimal preprocessing on the strings
     data['tweet'] = data['tweet'].apply(
         lambda w: w[2:len(w) - 2])  # remove [' and ']
+
+    v = process_sentence(data['tweet'])
 
     vitaly = data['tweet'].apply(lambda x: np.array(
         process_sentence(x)))
